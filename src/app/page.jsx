2c6ada8c826c323a306c33f28b08ee3e3b927ed3
SinoCodes01 <ngqazolosinovuyo50@gmail.com>
2026-02-05 16:17:03 +0200

    Date update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,7 +14,7 @@ export default function Home() {
   const [isLoading, setIsLoading] = useState(true)
   const [bubbles, setBubbles] = useState([])
   const [showForYouBtn, setShowForYouBtn] = useState(false)
-  const birthdayDate = new Date("February 05, 2026") // Change this date accordingly
+  const birthdayDate = new Date("February 25, 2026") // Change this date accordingly
   const audioRef = useRef(null)
 
   // For testing
